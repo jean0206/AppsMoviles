f7e56de3c455f43b9fce6f8ed11508ebb503f3a9
Nicolas Medina P <nickmedina99@hotmail.com>
2021-02-18 18:13:38 -0500

    hice la tarea je

--- class1/app/src/main/java/com/example/class1/MainActivity.java
+++ class1/app/src/main/java/com/example/class1/MainActivity.java
@@ -3,12 +3,53 @@ package com.example.class1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button loginBtn;
+    private ImageButton showPassBtn;
+    private EditText passwordInput;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loginBtn = findViewById(R.id.loginBtn);
+        showPassBtn = findViewById(R.id.showPassBtn);
+        passwordInput = findViewById(R.id.passwordInput);
+        progressBar = findViewById(R.id.progressBar);
+        progressBar.setVisibility(View.GONE);
+
+        showPassBtn.setOnTouchListener(new View.OnTouchListener() {
+            public boolean onTouch(View v, MotionEvent event) {
+
+                switch ( event.getAction() ) {
+
+                    case MotionEvent.ACTION_UP:
+                        passwordInput.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                        break;
+
+                    case MotionEvent.ACTION_DOWN:
+                        passwordInput.setInputType(InputType.TYPE_CLASS_TEXT);
+                        break;
+                }
+                return true;
+            }
+        });
+
+        loginBtn.setOnClickListener(
+                (v) -> {
+                    progressBar.setVisibility(View.VISIBLE);
+                }
+        );
     }
 }
